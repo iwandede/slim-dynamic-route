253d660852c3ed6ff741d1fba9759447d0d7667e
Iwan Gunawan <iwan.gunawan81@gmail.com>
2015-10-21 19:25:35 +0700

    Edit Config

--- config.php
+++ config.php
@@ -3,10 +3,10 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 $capsule = new Capsule;
 $capsule->addConnection(array(
 		'driver' 	=> 'mysql',
-		'host' 		=> '127.0.0.1',
-		'database' 	=> 'boykepedia',
-		'username' 	=> 'root',
-		'password' 	=> 'rahasia',
+		'host' 		=> 'YOUR_HOST',
+		'database' 	=> 'YOUR_DATABASE',
+		'username' 	=> 'YOUR_USERNAME',
+		'password' 	=> 'YOUR_PASSWORD',
 		'charset'  	=> 'utf8',
     	'collation' => 'utf8_general_ci',
     	'prefix'    => ''
